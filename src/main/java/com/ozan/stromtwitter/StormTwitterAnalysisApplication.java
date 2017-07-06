8af1483152a2c2012708d7a572912e62475b2f13
huseyin <huseyinsurat@gmail.com>
2017-07-06 22:09:37 +0300

    Commit fix.

---
+++ src/main/java/com/ozan/stromtwitter/StormTwitterAnalysisApplication.java
@@ -0,0 +1,163 @@
+package com.ozan.stromtwitter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ozan.stromtwitter.model.HashtagCount;
+
+@Controller
+@SpringBootApplication
+public class StormTwitterAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormTwitterAnalysisApplication.class, args);
+		
+		openBrowser();
+	}
+	
+	public static void openBrowser(){
+		String url = "http://localhost:8181/index";
+		String os = System.getProperty("os.name").toLowerCase();
+	    Runtime rt = Runtime.getRuntime();
+		try{
+		    if (os.indexOf( "win" ) >= 0) {
+		        // this doesn't support showing urls in the form of "page.html#nameLink" 
+		        rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+		    }else if (os.indexOf( "mac" ) >= 0) {
+		        rt.exec( "open " + url);
+	        }else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+		        // Do a best guess on unix until we get a platform independent way
+		        // Build a list of browsers to try, in this order.
+		        String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+		       			             "netscape","opera","links","lynx"};		       
+		        // Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+		        StringBuffer cmd = new StringBuffer();
+		        for (int i=0; i<browsers.length; i++)
+		            cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");		        	
+		        rt.exec(new String[] { "sh", "-c", cmd.toString() });
+	           } else {
+	                return;
+	           }
+	       }catch (Exception e){
+	    	   return;
+	       }
+			return;
+	}
+	
+	@RequestMapping("/index")
+    public String index(Locale locale, Model model) {
+        return "index";
+    }
+	
+	@RequestMapping(value = "/makeAnalysis", method = RequestMethod.POST)
+    public String makeAnalysis(Locale locale, Model model,
+    		@RequestParam(value = "runDuration", required = true) int runDuration,
+    		@RequestParam(value = "keywords", required = true) String keywords) {
+
+		String executeCmd = "/home/huseyin/Documents/ozanbitirme/apache-storm-1.0.2/bin/storm jar "
+				+ "/home/huseyin/Documents/Git/storm/examples/storm-starter/target/storm-starter-2.0.0-SNAPSHOT.jar "
+				+ "org.apache.storm.starter.TwitterHashtagStorm "
+//				+ "10000 nba lebron";
+				+ Integer.toString(runDuration) + " "
+				+ keywords;
+		
+		Runtime rt = Runtime.getRuntime();
+		
+        try {
+        	
+			Process proc = rt.exec(executeCmd);
+			
+			StreamGobbler outputGobbler = new 
+	                StreamGobbler(proc.getInputStream(), "OUTPUT", "/home/huseyin/Desktop/OzanSonuc/output.txt");
+			
+			outputGobbler.start();
+			
+			int exitVal = proc.waitFor();
+	        System.out.println("ExitValue: " + exitVal);
+        
+        } catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        
+        ObjectMapper mapper = new ObjectMapper();
+        
+        List<HashtagCount> hashtags = new ArrayList<HashtagCount>();
+        
+        try{
+        	
+        	File hastagFile = new File("/home/huseyin/Desktop/OzanSonuc/hashtag.txt");
+        	BufferedReader br = new BufferedReader(new FileReader(hastagFile));
+        	
+			String line;
+            while ((line = br.readLine()) != null) {
+            	line = line.trim();
+            	HashtagCount hashtagCount = mapper.readValue(line, HashtagCount.class);
+            	hashtags.add(hashtagCount);
+            }
+        
+            br.close();
+        }catch (Exception e) {
+			
+		}
+        
+        model.addAttribute("hashtags", hashtags);
+        return "index";
+	}
+	
+	class StreamGobbler extends Thread
+	{
+	    InputStream is;
+	    String type;
+	    String backupPath;
+	    
+	    StreamGobbler(InputStream is, String type, String backupPath)
+	    {
+	        this.is = is;
+	        this.type = type;
+	        this.backupPath = backupPath;
+	    }
+	    
+	    public void run()
+	    {
+	        try
+	        {
+	            InputStreamReader isr = new InputStreamReader(is);
+	            BufferedReader br = new BufferedReader(isr);
+	            String line=null;
+	            
+	            if(backupPath != null){
+	            	PrintWriter writer = new PrintWriter(backupPath, "UTF-8");
+	        	    while ( (line = br.readLine()) != null){
+	        	    	writer.println(line);
+	        	    }
+	        	    writer.close();
+	            }else{
+	            	while ( (line = br.readLine()) != null){
+	        	    	System.out.println(line);
+	        	    }
+	            }
+	        } catch (IOException ioe){
+	        	ioe.printStackTrace();  
+	        }
+	    }
+	}
+}
---
+++ src/main/java/com/ozan/stromtwitter/model/HashtagCount.java
@@ -0,0 +1,32 @@
+package com.ozan.stromtwitter.model;
+
+public class HashtagCount {
+	
+	private String hashtag;
+	private int count;
+	
+	public HashtagCount(String hashtag, int count) {
+		super();
+		this.hashtag = hashtag;
+		this.count = count;
+	}
+
+	public String getHashtag() {
+		return hashtag;
+	}
+
+	public void setHashtag(String hashtag) {
+		this.hashtag = hashtag;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+
+}
